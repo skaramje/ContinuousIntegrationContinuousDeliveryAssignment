2bf1096bcc06b563a7786b7fec7427d811a49d87
skaramje <skaramje@gmail.com>
2022-04-03 03:38:52 -0600

    auto deploy

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("listening on port 3000!");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
